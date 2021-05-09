021f67b842f5ef6d49ea7c561fd010b5ac2f7e80
remichartier <remipr.chartier@gmail.com>
2021-05-09 01:26:49 -0700

    Completed UKF Sigma Point Augmentation Quiz/Exercize

--- 002_GeneratingSigmaPointsUKFAugmentation/ukf.cpp
+++ 002_GeneratingSigmaPointsUKFAugmentation/ukf.cpp
@@ -103,13 +103,25 @@ void UKF::AugmentedSigmaPoints(MatrixXd* Xsig_out) {
    */
  
   // create augmented mean state
-
-  // create augmented covariance matrix
-
-  // create square root matrix
-
-  // create augmented sigma points
+  x_aug.head(n_x) = x;
+  x_aug(5) = 0.0;
+  x_aug(6) = 0.0;
   
+  // create augmented covariance matrix
+  P_aug.topLeftCorner(n_x, n_x)= P; 
+  P_aug(5,5) = std_a * std_a;
+  P_aug(6,6) = std_yawdd * std_yawdd;
+  
+  // create square root matrix
+  MatrixXd A = P_aug.llt().matrixL();
+  // create augmented sigma points
+  Xsig_aug.col(0) = x_aug;
+
+  // set sigma points as columns of matrix Xsig
+  for(int i=0; i < n_aug; i++){
+    Xsig_aug.col(i+1)     = x_aug + sqrt(lambda + n_aug) * A.col(i);
+    Xsig_aug.col(i+1+n_aug) = x_aug - sqrt(lambda + n_aug) * A.col(i);
+  }
   /**
    * Student part end
    */
@@ -132,3 +144,14 @@ void UKF::AugmentedSigmaPoints(MatrixXd* Xsig_out) {
  *      0        0        0        0        0        0  0.34641        0        0        0        0        0        0 -0.34641        0
  *      0        0        0        0        0        0        0  0.34641        0        0        0        0        0        0 -0.34641
  */
+
+/* Obtained : 
+Xsig_aug = 
+  5.7441  5.85768   5.7441   5.7441   5.7441   5.7441   5.7441   5.7441  5.63052   5.7441   5.7441   5.7441   5.7441   5.7441   5.7441
+    1.38  1.34566  1.52806     1.38     1.38     1.38     1.38     1.38  1.41434  1.23194     1.38     1.38     1.38     1.38     1.38
+  2.2049  2.28414  2.24557  2.29582   2.2049   2.2049   2.2049   2.2049  2.12566  2.16423  2.11398   2.2049   2.2049   2.2049   2.2049
+  0.5015  0.44339 0.631886 0.516923 0.595227   0.5015   0.5015   0.5015  0.55961 0.371114 0.486077 0.407773   0.5015   0.5015   0.5015
+  0.3528 0.299973 0.462123 0.376339  0.48417 0.418721   0.3528   0.3528 0.405627 0.243477 0.329261  0.22143 0.286879   0.3528   0.3528
+       0        0        0        0        0        0  0.34641        0        0        0        0        0        0 -0.34641        0
+       0        0        0        0        0        0        0  0.34641        0        0        0        0        0        0 -0.34641
+*/
