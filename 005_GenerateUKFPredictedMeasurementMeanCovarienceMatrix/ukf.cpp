e5ac75db32ebf03c6a29aec61040b62c32c253e5
remichartier <remipr.chartier@gmail.com>
2021-05-12 01:13:13 -0700

    UKF Measurement prediction mean and covariance implemented

--- 005_GenerateUKFPredictedMeasurementMeanCovarienceMatrix/ukf.cpp
+++ 005_GenerateUKFPredictedMeasurementMeanCovarienceMatrix/ukf.cpp
@@ -17,6 +17,42 @@ void UKF::Init() {
 }
 
 /**
+       • https://youtu.be/GYQeizoj09E
+    • Goal coding Quiz is to predict the mean and the covariance of a radar
+     measurement.
+    • Expect Radar measurement → measurement dimension to 3.
+    • Gave example on how set the weights.
+    • To be able to build the measurement covariance matrix R, gave values
+     for radar measurement uncertainty.
+    • Predicted Sigma Points given.
+    • Zsig to store measurement Sigma points.
+    • 2 output objects : z_pred mean predicted measurement, and S the predicted
+     covariance matrix S.
+
+ */
+
+// compilation : g++ main.cpp ukf.cpp -o main.exe
+
+/* Dense not found --> due to Eigen files/lib not installed 
+  From : https://eigen.tuxfamily.org/dox/GettingStarted.html
+   Compiling and running your first program
+  There is no library to link to. The only thing that you need to keep in mind when compiling the above program is that the compiler must be able to find the Eigen header files. The directory in which you placed Eigen's source code must be in the include path. With GCC you use the -I option to achieve this, so you can compile the program with a command like this:
+
+  g++ -I /path/to/eigen/ my_program.cpp -o my_program 
+  On Linux or Mac OS X, another option is to symlink or copy the Eigen folder into /usr/local/include/. This way, you can compile the program with:
+
+  g++ my_program.cpp -o my_program 
+  
+  https://askubuntu.com/questions/860207/how-to-install-eigen-3-3-in-ubuntu-14-04
+  For those simply requiring a reasonably recent version of Eigen 3 on Ubuntu
+   and similar Debian-based distros (...which is the common case), installing 
+   the existing libeigen3-dev package suffices: e.g.,
+
+  sudo apt install libeigen3-dev
+
+*/
+
+/**
  * Programming assignment functions: 
  */
 
@@ -76,11 +112,50 @@ void UKF::PredictRadarMeasurement(VectorXd* z_out, MatrixXd* S_out) {
    */
 
   // transform sigma points into measurement space
+  VectorXd x = VectorXd(n_x);
+  VectorXd z = VectorXd(n_z);
+  
+  for(int i=0; i<2 * n_aug + 1; i++){
+	x = Xsig_pred.col(i);
+	// Column i
+	double px(x(0)), py(x(1)), v(x(2)), psi(x(3)), psi_dot(x(4));
+	double rho = sqrt(px*px + py*py);
+	if(px==0){
+		std::cout << "px=0 division by 0" << std::endl;
+	  	return;
+	}
+	double phi = atan2(py,px);
+	if(rho==0){
+	  	std::cout << "sqrt(px*px + py*py)=0 division by 0" << std::endl;
+	  	return;
+	}
+	double phi_dot = v*(px*cos(psi) + py*sin(psi))/rho;
+	z(0) = rho;
+	z(1) = phi;
+	z(2) = phi_dot;
+	Zsig.col(i) = z;  	
+  }
   
   // calculate mean predicted measurement
+  z_pred = weights(0)*Zsig.col(0);
+  for(int i=1; i<2 * n_aug + 1; i++){
+  	z_pred = z_pred + weights(i)*Zsig.col(i);
+  }
   
   // calculate innovation covariance matrix S
+  MatrixXd R = MatrixXd(n_z,n_z);
+  R <<
+  		std_radr*std_radr, 	0, 						0,
+  		0, 					std_radphi*std_radphi,	0,
+  		0,					0,						std_radrd*std_radrd;
 
+  S.Zero(n_z,n_z);
+  VectorXd diff = VectorXd(n_z);
+  for(int i=0; i<2 * n_aug + 1; i++){
+  	diff = (Zsig.col(i) - z_pred);
+  	S = S + (weights(i)*diff*(diff.transpose()));
+  }
+  S = S + R;
   /**
    * Student part end
    */
